0faa40891b98655ec6edffebeb19c3ec01687c55
yasosulakshan8-jpg <yasosulakshan8@gmail.com>
2026-05-16 17:17:02 +0530

    Add files via upload

---
+++ 1114/Javascript/Operators/operators.js
@@ -0,0 +1,15 @@
+== -> Equal to (loose) //only the value
+
+=== -> Equal to(strict) //value and the data type
+
+!= -> Not equal to (loose)
+
+!== -> Not equal to (strict)
+
+> -> Greater than
+
+< -> Less than
+
+>= -> Grater than or equal to 
+
+<= -> Less than or equal to
